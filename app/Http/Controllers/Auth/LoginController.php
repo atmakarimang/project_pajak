66a166d7fd1d944d2227bfca7878d25a18397e43
atmakarimang <atmakarimang@gmail.com>
2022-02-26 00:04:07 +0700

    bug path view again

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -11,33 +11,35 @@ use Session;
 
 class LoginController extends Controller
 {
-    public function index(){
-		if (Auth::check()) {
+    public function index()
+    {
+        if (Auth::check()) {
             // return redirect()->route('dashboard');
             return redirect()->route('pelaksanabidang.index');
         }
-    	return view('Auth.login');
+        return view('auth.login');
     }
     public function showLoginForm()
     {
-      return view('Auth.login');
+        return view('auth.login');
     }
-    public function proses_login(Request $request){
+    public function proses_login(Request $request)
+    {
         $user_id = $request->user_id;
         $password = $request->password;
-        if(!empty(session('user_id'))) {
+        if (!empty(session('user_id'))) {
             // return redirect()->route('dashboard');
             return redirect()->route('pelaksanabidang.index');
         }
 
-        if(empty($user_id) || empty($password)) {
-            return view('Auth.login');
+        if (empty($user_id) || empty($password)) {
+            return view('auth.login');
         }
 
-        $user = User::where("user_id", '=',$user_id)
-                    ->where("password", '=',md5($password))
-                    ->first();
-        if(!empty($user)) {
+        $user = User::where("user_id", '=', $user_id)
+            ->where("password", '=', md5($password))
+            ->first();
+        if (!empty($user)) {
             session(['user_id' => $user->user_id]);
             // return redirect()->route('dashboard');
             return redirect()->route('pelaksanabidang.index');
@@ -46,9 +48,10 @@ class LoginController extends Controller
             return redirect()->back();
         }
     }
-    public function logout(Request $request){
-    	$request->session()->flush();
-    	Auth::logout();
-    	return redirect('login');
+    public function logout(Request $request)
+    {
+        $request->session()->flush();
+        Auth::logout();
+        return redirect('login');
     }
 }
