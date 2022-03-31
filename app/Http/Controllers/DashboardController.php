15f7986940df1d2f5b59af502b701b58d2551fcc
atmakarimang <atmakarimang@gmail.com>
2022-03-31 08:41:03 +0700

    update chart v1

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth; // baru
 use App\Models\User;
+use App\Models\JenisPermohonan;
 
 class DashboardController extends Controller
 {
@@ -12,13 +13,35 @@ class DashboardController extends Controller
     // {
     //     $this->middleware('auth');
     // }
-    public function index(){
-        $data = [];
-        $user = Auth::user();
-        $user_id = session('user_id');
-        
-        $data['user'] = $user;
+    // public function index(){
+    //     $data = [];
+    //     $user = Auth::user();
+    //     $user_id = session('user_id');
 
-        return view('dashboard.index',$data);
+    //     $data['user'] = $user;
+
+    //     return view('dashboard.index',$data);
+    // }
+
+    public function index()
+    {
+        $permohonan = JenisPermohonan::get()->toArray();
+
+        $permohonanid = [];
+        foreach ($permohonan as $key => $value) {
+            //$years .= $permohonan[$key]['id'];
+            array_push($permohonanid, $permohonan[$key]['jenis_permohonan']);
+        }
+
+        $year = ['2015', '2016', '2017', '2018', '2019', '2020'];
+
+        //print_r($year);
+
+        $user = [10, 20, 30, 40, 30, 60];
+        // foreach ($year as $key => $value) {
+        //     $user[] = User::where(\DB::raw("DATE_FORMAT(created_at, '%Y')"),$value)->count();
+        // }
+
+        return view('dashboard.index')->with('year', json_encode($permohonanid, JSON_NUMERIC_CHECK))->with('user', json_encode($user, JSON_NUMERIC_CHECK));
     }
 }
--- resources/views/dashboard/html.blade.php
+++ resources/views/dashboard/html.blade.php
@@ -4,7 +4,7 @@
           <!-- Main row -->
           <div class="row">
             <!-- Left col -->
-            <section class="col-lg-7 connectedSortable">
+            <section class="col-lg-12 connectedSortable">
               <!-- Custom tabs (Charts with tabs)-->
               <div class="card">
                 <div class="card-header">
@@ -38,7 +38,7 @@
             </section>
             <!-- /.Left col -->
             <!-- right col (We are only adding the ID to make the widgets sortable)-->
-            <section class="col-lg-5 connectedSortable">
+            {{-- <section class="col-lg-5 connectedSortable">
 
               <!-- Map card -->
               <div class="card bg-gradient-primary">
@@ -117,7 +117,7 @@
                 <!-- /.card-footer -->
               </div>
               <!-- /.card -->
-            </section>
+            </section> --}}
             <!-- right col -->
           </div>
           <!-- /.row (main row) -->
@@ -125,44 +125,156 @@
       </section>
       <!-- /.content -->
       <script>
-        console.log("AAAA");
-      </script>
-      <script>
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
+        // var ctx = document.getElementById('myChart').getContext('2d');
+        // var myChart = new Chart(ctx, {
+        //     type: 'bar',
+        //     data: {
+        //         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        //         datasets: [{
+        //             label: '# of Votes',
+        //             data: [12, 19, 3, 5, 2, 3],
+        //             backgroundColor: [
+        //                 'rgba(255, 99, 132, 0.2)',
+        //                 'rgba(54, 162, 235, 0.2)',
+        //                 'rgba(255, 206, 86, 0.2)',
+        //                 'rgba(75, 192, 192, 0.2)',
+        //                 'rgba(153, 102, 255, 0.2)',
+        //                 'rgba(255, 159, 64, 0.2)'
+        //             ],
+        //             borderColor: [
+        //                 'rgba(255, 99, 132, 1)',
+        //                 'rgba(54, 162, 235, 1)',
+        //                 'rgba(255, 206, 86, 1)',
+        //                 'rgba(75, 192, 192, 1)',
+        //                 'rgba(153, 102, 255, 1)',
+        //                 'rgba(255, 159, 64, 1)'
+        //             ],
+        //             borderWidth: 1
+        //         }]
+        //     },
+        //     options: {
+        //         scales: {
+        //             yAxes: [{
+        //                 ticks: {
+        //                     beginAtZero: true
+        //                 }
+        //             }]
+        //         }
+        //     }
+        // });
+
+        // var year = <?php echo $year; ?>;
+        // var user = <?php echo $user; ?>;
+        // var barChartData = {
+        //     labels: year,
+        //     datasets: [{
+        //         label: 'User',
+        //         backgroundColor: "pink",
+        //         data: user
+        //     }]
+        // };
+
+        // window.onload = function() {
+        //     var ctx = document.getElementById("myChart").getContext("2d");
+        //     window.myBar = new Chart(ctx, {
+        //         type: 'bar',
+        //         data: barChartData,
+        //         options: {
+        //             elements: {
+        //                 rectangle: {
+        //                     borderWidth: 2,
+        //                     borderColor: '#c1c1c1',
+        //                     borderSkipped: 'bottom'
+        //                 }
+        //             },
+        //             responsive: true,
+        //             title: {
+        //                 display: true,
+        //                 text: 'Yearly User Joined'
+        //             }
+        //         }
+        //     });
+        // };
+
+        var year = <?php echo $year; ?>;
+        var user = <?php echo $user; ?>;
+        var barChartData = {
+          labels: year,
+          datasets: [
+            {
+              //label: 'User',
+              backgroundColor: "#007bff",
+              borderColor: "#007bff",
+              data: user
             },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
+            {
+              //label: 'User',
+              backgroundColor: "#ced4da",
+              borderColor: "#ced4da",
+              data: user
             }
+          ]
+        };
+
+        $(function () {
+          "use strict";
+          var ticksStyle = { fontColor: "#495057", fontStyle: "bold" };
+          var mode = "index";
+          var intersect = true;
+          var $salesChart = $("#myChart");
+          var salesChart = new Chart($salesChart, {
+            type: "bar",
+            data: barChartData,
+            // data: {
+            //   labels: ["JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
+            //   datasets: [
+            //     {
+            //       backgroundColor: "#007bff",
+            //       borderColor: "#007bff",
+            //       data: [1000, 2000, 3000, 2500, 2700, 2500, 3000],
+            //     },
+            //     {
+            //       backgroundColor: "#ced4da",
+            //       borderColor: "#ced4da",
+            //       data: [700, 1700, 2700, 2000, 1800, 1500, 2000],
+            //     },
+            //   ],
+            // },
+            options: {
+              maintainAspectRatio: false,
+              tooltips: { mode: mode, intersect: intersect },
+              hover: { mode: mode, intersect: intersect },
+              legend: { display: false },
+              scales: {
+                yAxes: [
+                  {
+                    gridLines: {
+                      display: true,
+                      lineWidth: "4px",
+                      color: "rgba(0, 0, 0, .2)",
+                      zeroLineColor: "transparent",
+                    },
+                    ticks: $.extend(
+                      {
+                        beginAtZero: true,
+                        callback: function (value) {
+                          if (value >= 1000) {
+                            value /= 1000;
+                            value += "k";
+                          }
+                          return "$" + value;
+                        },
+                      },
+                      ticksStyle
+                    ),
+                  },
+                ],
+                xAxes: [
+                  { display: true, gridLines: { display: false }, ticks: ticksStyle },
+                ],
+              },
+            },
+          });
         });
+
         </script>
